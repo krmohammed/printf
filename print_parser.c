75a639e2c72287202e9acc48ac23d1abb457ae79
mohrahman <mohrahman558@gmail.com>
2023-06-27 02:02:09 +0000

    hot fixes

--- print_parser.c
+++ print_parser.c
@@ -14,7 +14,7 @@ int output_parser(const char *format, format_t format_list[], va_list *args)
 	int printed = 0;
 	unsigned int count = 0, i = 0, j = 0;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (format[i] != '\0')
 	{
